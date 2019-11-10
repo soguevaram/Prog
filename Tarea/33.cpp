d0c3f8e91c331a97aa941719558e380168625e58
soguevaram <54988568+soguevaram@users.noreply.github.com>
2019-11-10 18:30:57 -0500

    For auto const

---
+++ Tarea/33.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main (void)
+{
+  std::vector <double> cities = {234523.56,1543476.6,36543.6,325.6};
+  
+  for (auto const &element: cities)
+    {
+      double sum = element;
+    }
+}
+
