468da7c997e4c23c7ce0b7a818f653a3e08dcb3b
soguevaram <54988568+soguevaram@users.noreply.github.com>
2019-11-10 20:19:41 -0500

    Cities, sum, average, max and min

--- Tarea/33.cpp
+++ Tarea/33.cpp
@@ -4,10 +4,44 @@
 int main (void)
 {
   std::vector <double> cities = {234523.56,1543476.6,36543.6,325.6};
+
+  double sum = 0.0;
+  double average = 0.0;
   
-  for (auto const &element: cities)
+  for (double ii = 0; ii < cities.size(); ++ii)
     {
-      double sum = element;
+      sum += cities [ii];
+      average = sum/cities.size();
     }
+
+  double mayor = 0.0;
+  double menor = 0.0;
+
+  for (double ii = 0; ii < cities.size (); ++ii)
+    {
+      if (cities [ii] > mayor)
+	{
+	  mayor = cities [ii];
+	}
+    }
+  
+  for (double ii = 0; ii < cities.size (); ++ii)
+    {
+      menor = cities [ii];
+      
+      if (cities [ii] < menor)
+	{
+	  menor = cities [ii];
+	}
+    }
+
+  std::cout << sum << "\n";
+  std::cout << mayor << "\n";
+  std::cout << menor << "\n";
+  std::cout << average << "\n";
+  
+  
+  return 0;
+  
 }
 
