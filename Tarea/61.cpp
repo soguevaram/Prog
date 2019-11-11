1b41ae6be74f3ad6421b08d8a3e4589a9cce2e95
soguevaram <54988568+soguevaram@users.noreply.github.com>
2019-11-11 17:34:06 -0500

    Escribe un numero y suelta la palabra

---
+++ Tarea/61.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main (void)
+
+{
+  int number = 0;
+  std::vector <std::string> numbers = {"Zero", "One","Two", "Three", "Four", "Five","Six","Seven", "Eight", "Nine"};
+
+  std::cout << "Write a number" << "\n";
+  std::cin >> number;
+
+  if (number == 0)
+    {
+      std::cout << numbers [0] << "\n";
+    }
+
+  if (number == 1)
+    {
+      std::cout << numbers [1] << "\n";
+    }
+
+  if (number == 2)
+    {
+      std::cout << numbers [2] << "\n";
+    }
+
+  if (number == 3)
+    {
+      std::cout << numbers [3] << "\n";
+    }
+
+  if (number == 4)
+    {
+      std::cout << numbers [4] << "\n";
+    }
+
+  if (number == 5)
+    {
+      std::cout << numbers [5] << "\n";
+    }
+
+  if (number == 6)
+    {
+      std::cout << numbers [6] << "\n";
+    }
+
+  if (number == 7)
+    {
+      std::cout << numbers [7] << "\n";
+    }
+
+  if (number == 8)
+    {
+      std::cout << numbers [8] << "\n";
+    }
+
+   if (number == 9)
+    {
+      std::cout << numbers [9] << "\n";
+    }
+  
+  
+  
+}
